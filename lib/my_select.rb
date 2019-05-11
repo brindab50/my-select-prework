4581293404097aa009b92ae15892ecac3692c7b3
brinda behal <brindab50@gmail.com>
2019-05-11 20:56:23 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,12 @@
+require 'pry'
 def my_select(collection)
   new_collection = []
   i = 0
   while i < collection.length
+  bindind.pry
   if  yield(collection[i]) 
   new_collection  << collection[i]
-    
-  end
+    end
   i = i + 1 
 end
 new_collection
