a4e4cc2c8ddf52a3191bc7c89d99f39861c1bcf4
brinda behal <brindab50@gmail.com>
2019-05-11 20:37:57 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   new_collection = []
   i = 0
   while i < collection.length
-  new_collection.select << yield(my_select)
+  new_collection.select
   yield collection[1]
     i = i + 1 
 end
